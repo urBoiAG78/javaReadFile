4aaceaca3c639f8a22fb2a7297fd67a1eb204141
agonzalez <agonzalez@anthonys-air.columbus.rr.com>
2017-11-20 12:30:59 -0500

    le
    mayo

--- src/javareadfile/JavaReadFile.java
+++ src/javareadfile/JavaReadFile.java
@@ -5,6 +5,8 @@
  */
 package javareadfile;
 
+import java.io.*;
+import java.util.*;
 /**
  *
  * @author agonzalez
@@ -15,7 +17,39 @@ public class JavaReadFile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        ArrayList<String> bNames = new ArrayList<>();
+        
+        try(BufferedReader br = new BufferedReader(new FileReader("BoyNames.txt")))
+        {
+            for(int i = 0; i < 200; i++)
+            {
+                bNames.add(br.readLine());
+            }
+            Collections.sort(bNames);
+            System.out.println(bNames);
+        }
+        catch(IOException ex)
+        {
+            System.out.println("Had trouble reading file.");
+            System.exit(0);
+        }
+        
+        try(FileWriter writer = new FileWriter("SortedBoyNames.txt"))
+        {
+            for(String str: bNames) 
+            {
+                writer.write(str + ", ");
+            }
+            
+        }
+        catch(IOException ex)
+        {
+            System.out.println("Had trouble sorting or reading file.");
+            System.exit(0);
+        }
+    }
     }
     
-}
+    
+
